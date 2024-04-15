fa3b3f99048ac19712ab537bb61b3e11a6fe6ee5
eliansoutu <62449092+eliansoutu@users.noreply.github.com>
2024-04-14 21:58:35 -0300

    Add files via upload

---
+++ app.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(dundermifflin)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+  br(),
+  br(),
+  br(),
+  
+  column(offset = 5, width = 2,
+  actionButton("boton", "Haceme click wacho")
+  ),
+  
+
+  
+  column(offset = 2, width = 8,
+         br(),  br(),tableOutput("citaOffice")
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  observeEvent(input$boton, {
+    
+    output$citaOffice <- renderTable({
+      
+      get_quote()
+      
+    })
+    
+  })
+  
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
